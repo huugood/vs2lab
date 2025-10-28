bba1023e97d4b609728631fc755d82a7a4dd154a
Hugo Dünger <hugo.duenger@gmail.com>
2025-10-28 19:54:37 +0000

    improve protocol to allow for messages larger than 1024 bytes

--- lab1/clientserver.py
+++ lab1/clientserver.py
@@ -4,6 +4,7 @@ Client and server using classes
 
 import logging
 import socket
+import struct
 
 import const_cs
 from context import lab_logging
@@ -15,6 +16,19 @@ lab_logging.setup(stream_level=logging.INFO)  # init loging channels for the lab
 
 # pylint: disable=logging-not-lazy, line-too-long
 
+
+def recv_all(connection: socket.socket, n: int) -> bytes | None:
+    """Helper function to reliably receive exactly n bytes."""
+    data: bytes = b''
+    while len(data) < n:
+        bytes_needed = n - len(data)
+        chunk = connection.recv(bytes_needed)
+        if not chunk:
+            return None
+        data += chunk
+    return data
+
+
 class Server:
     """ The server """
     _logger = logging.getLogger("vs2lab.lab1.clientserver.Server")
@@ -25,16 +39,16 @@ class Server:
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # prevents errors due to "addresses in use"
         self.sock.bind((const_cs.HOST, const_cs.PORT))
         self.sock.settimeout(3)  # time out in order not to block forever
-        self._logger.info("Server bound to socket " + str(self.sock))
+        self._logger.info("Server bound to socket %s", self.sock)
 
-    def get_tel(self, name:str) -> str|None:
-        self._logger.info('getting number for: ' + name)
+    def get_tel(self, name: str) -> str | None:
+        self._logger.info('getting number for: %s', name)
         return tel.get(name)
-    
-    def format_get_result(self, number: str|None) -> str:
+
+    def format_get_result(self, number: str | None) -> str:
         self._logger.info('formatting result')
-        command:str = ''
-        msg:str = '\n'
+        command: str = ''
+        msg: str = '\n'
         if not number:
             command = 'NOT_FOUND'
             msg = ''
@@ -46,59 +60,68 @@ class Server:
 
     def getall_tel(self) -> list[tuple[str, str]]:
         return list(tel.items())
-    
+
     def format_getall_result(self, numbers: list[tuple[str, str]]) -> str:
         self._logger.info('formatting result')
-        command:str = 'ENTRIES'
+        command: str = 'ENTRIES'
 
         numbers_str = [f"{name}: {number}" for name, number in numbers]
         msg = '\n' + ';'.join(numbers_str)
 
         return command + msg
-        
-
 
     def serve(self):
-        print(tel.items())
-
         """ Serve echo """
+        print(tel.items())
         self.sock.listen(1)
 
         while self._serving:  # as long as _serving (checked after connections or socket timeouts)
             try:
-                # pylint: disable=unused-variable
-                (connection, address) = self.sock.accept()  # returns new socket and address of client
+                (connection, _) = self.sock.accept()  # returns new socket and address of client
                 while True:  # forever
-                    data = connection.recv(1024)  # receive data from client
-                    if not data:
+                    len_prefix: bytes | None = recv_all(connection, 4)  # read length prefix first
+                    if not len_prefix:
+                        self._logger.info("Connection closed by client.")
                         break
-                    self._logger.info("Message received: " + repr(data.decode('ascii')))
-                    data = data.decode('ascii')
+                    self._logger.info("Received message length prefix, unpacking ...")
+                    (msg_len,) = struct.unpack('!I', len_prefix)
+                    self._logger.info(f"Message length is: {msg_len} bytes. Now receiving full message ...")
+                    data_bytes = recv_all(connection, msg_len)
+                    if not data_bytes:
+                        self._logger.error("Connection closed unexpectedly while receiving message body.")
+                        break
+                    data = data_bytes.decode('ascii')
+                    self._logger.info("Message received: %r", data)
                     data_lines = data.splitlines()
                     command = data_lines[0]
-                    self._logger.info("Command: " +  command)
+                    self._logger.info("Command: %s", command)
+
+                    # Determine response message
+                    formatted_msg: str
                     if command == 'GET':
-                        query_param:str = data_lines[1]
+                        query_param: str = data_lines[1]
                         tel_result = self.get_tel(query_param)
                         formatted_msg = self.format_get_result(tel_result)
-                        self._logger.info("Sending message")
-                        connection.send(formatted_msg.encode('ascii'))
                     elif command == 'GETALL':
                         tel_result = self.getall_tel()
                         formatted_msg = self.format_getall_result(tel_result)
-                        self._logger.info("Sending message")
-                        connection.send(formatted_msg.encode('ascii'))
                     else:
-                        self._logger.warning("Command " + command + " not supported")
-                        connection.send('ERR\nCommand not supported'.encode('ascii'))
+                        self._logger.warning("Command %s not supported", command)
+                        formatted_msg = 'ERR\nCommand not supported'
+                    
+                    # Encode message and prepend length prefix before sending
+                    msg_bytes_out = formatted_msg.encode('ascii')
+                    len_prefix_out = struct.pack('!I', len(msg_bytes_out))
+                    full_message_out = len_prefix_out + msg_bytes_out
+                    
+                    self._logger.info("Sending message with length %d", len(msg_bytes_out))
+                    connection.sendall(full_message_out)
+
                 connection.close()  # close the connection
             except socket.timeout:
                 pass  # ignore timeouts
         self.sock.close()
         self._logger.info("Server down.")
-
-    
-    
 
 
 class Client:
@@ -108,19 +131,42 @@ class Client:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((const_cs.HOST, const_cs.PORT))
-        self.logger.info("Client connected to socket " + str(self.sock))
+        self.logger.info("Client connected to socket %s", self.sock)
 
-    def call(self, msg_in:str="GE\njacka"):
+    def call(self, msg_in: str = "GETALL"):
         """ Call server """
-        self.sock.send(msg_in.encode('ascii'))  # send encoded string as data
-        data = self.sock.recv(1024)
-        self.logger.info("Message received: " + repr(data.decode('ascii')))
-        data = data.decode('ascii')
+
+        # Encode message and prepend length prefix
+        msg_bytes = msg_in.encode('ascii')
+        msg_len = len(msg_bytes)
+        self.logger.info("Msg length %r", msg_len)
+        len_prefix = struct.pack('!I', msg_len)  # creates a 4-byte unsigned integer
+        full_message = len_prefix + msg_bytes
+
+        self.logger.info("Sending message: %r", full_message)
+        self.sock.sendall(full_message)  # sendall repeatedly tries to send until all data is sent
+
+        # Receive response: first get length prefix, then the message body
+        len_prefix_in = recv_all(self.sock, 4)
+        if not len_prefix_in:
+            print("Server closed connection unexpectedly.")
+            return "ERR"
+        
+        (msg_len_in,) = struct.unpack('!I', len_prefix_in)
+        self.logger.info("Expecting %d bytes from server.", msg_len_in)
+
+        data_bytes = recv_all(self.sock, msg_len_in)
+        if not data_bytes:
+            print("Server closed connection while sending message body.")
+            return "ERR"
+
+        self.logger.info("Message received: %r", data_bytes.decode('ascii'))
+        data = data_bytes.decode('ascii')
         data_lines = data.splitlines()
 
         command = data_lines[0]
-        self.logger.info("Command: " +  command)
-        msg:str = ''
+        self.logger.info("Command: %s", command)
+        msg: str = ''
         if command == 'FOUND':
             msg = data_lines[1]
         elif command == 'NOT_FOUND':
@@ -131,16 +177,15 @@ class Client:
             msg = data_lines[1]
         else:
             msg = 'Weird response from server'
-            self.logger.warning("Command " + command + " not supported")
-            
+            self.logger.warning("Command %s not supported", command)
 
         print(msg)
         self.sock.close()  # close the connection
         self.logger.info("Client down.")
         return msg
-    
-    def send_get(self, name:str):
-        self.call('GET\n'+name)
+
+    def send_get(self, name: str):
+        self.call('GET\n' + name)
 
     def send_getall(self):
         self.call('GETALL')
--- lab1/tel.py
+++ lab1/tel.py
@@ -1,4 +1,26 @@
+# data_generator.py
+
+import random
+
+# Initialize the dictionary with the original data
 tel = {
     'jack': '4098',
     'sape': '4139'
-    }
+}
+
+# 1. Determine the number of new entries needed
+# Need 1000 - 2 = 998 new entries.
+num_new_entries = 1000 - len(tel)
+
+# 2. Generate the new entries and update the dictionary
+for i in range(1, num_new_entries + 1):
+    # Create a unique key (e.g., 'user_1', 'user_2', ...)
+    key = f'user_{i}'
+
+    # Generate a random 9-digit number as a string (100000000 to 999999999)
+    value = str(random.randint(100000000, 999999999))
+
+    # Add the new key-value pair to the dictionary
+    tel[key] = value
+
+# The dictionary 'tel' now contains 1000 entries.
